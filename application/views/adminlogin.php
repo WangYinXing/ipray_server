d36bb0f82ed54884ff1cb5c4c14d9f5ccbe93c9c
wangyinxing <wangyinxing19@gmail.com>
2016-04-12 11:53:08 +0800

    no message

--- application/views/adminlogin.php
+++ application/views/adminlogin.php
@@ -12,15 +12,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 	
 	<!-- Bootstrap 3.3.5 -->
-	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="/assets/bootstrap/css/bootstrap.min.css">
 	<!-- Theme style -->
-	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/dist/css/AdminLTE.min.css">
+	<link rel="stylesheet" href="/assets/dist/css/AdminLTE.min.css">
 	<!-- iCheck -->
-	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/iCheck/square/blue.css">
+	<link rel="stylesheet" href="/assets/plugins/iCheck/square/blue.css">
 
-	<script src="<?php echo base_url(); ?>assets/plugins/jQuery/jQuery-2.1.4.min.js"></script>
-	<script src="<?php echo base_url(); ?>assets/bootstrap/js/bootstrap.min.js"></script>
-	<script src="<?php echo base_url(); ?>assets/plugins/iCheck/icheck.min.js"></script>
+	<script src="/assets/plugins/jQuery/jQuery-2.1.4.min.js"></script>
+	<script src="/assets/bootstrap/js/bootstrap.min.js"></script>
+	<script src="/assets/plugins/iCheck/icheck.min.js"></script>
 
 	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/dist/css/common.css">
   <link rel="stylesheet" href="<?php echo base_url(); ?>assets/dist/css/skin-custom.css">
